8c37238c46024ea49fca739d999eaf5957fc07aa
Devanshu <devanshusingh530@gmail.com>
2025-02-06 15:27:21 +0530

    new patterns

--- practice.cpp
+++ practice.cpp
@@ -1,9 +1,17 @@
+// To print a triangle with continuos integers 
 #include<iostream>
 using namespace std;
-int main(){
-int n;
-cout<<"Enter a number: ";
-cin >>n;
-
+int main()
+{
+int n,num=1;
+cout << "Enter the number of rows to print integers :";
+cin >> n;
+for(int i=1;i<=n;i++){
+    for (int j=1; j<=i; j++){
+        cout <<  num << " ";
+        num++;
+    }
+cout << endl;
+}
 return 0;
 }
