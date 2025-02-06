d8fbb188fcdfb750952d7108bb9480f9167ade49
Devanshu <devanshusingh530@gmail.com>
2025-02-06 15:57:09 +0530

    still new

--- practice.cpp
+++ practice.cpp
@@ -1,17 +1,21 @@
-// To print a triangle with continuos integers 
 #include<iostream>
 using namespace std;
 int main()
 {
-int n,num=1;
-cout << "Enter the number of rows to print integers :";
+int n;
+cout <<"Enter the number of rows and coloumns:";
 cin >> n;
-for(int i=1;i<=n;i++){
-    for (int j=1; j<=i; j++){
-        cout <<  num << " ";
-        num++;
+  for (int i = 1; i <= n; i++) {
+        // Print spaces
+        for (int j = 0; j < n - i; j++) {
+            cout << " ";
+        }
+        // Print stars
+        for (int j = 0; j < 2 * i - 1; j++) {
+            cout << "*";
+        }
+        cout << endl;
     }
-cout << endl;
-}
 return 0;
 }
+
