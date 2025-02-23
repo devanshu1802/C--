911de8a760169098dc3ca9e04b6817aa69defe90
Devanshu <devanshusingh530@gmail.com>
2025-02-23 07:39:34 +0530

    d

--- practice.cpp
+++ practice.cpp
@@ -1,21 +1,32 @@
 #include<iostream>
 using namespace std;
-int main()
-{
-int n;
-cout <<"Enter the number of rows and coloumns:";
-cin >> n;
-  for (int i = 1; i <= n; i++) {
-        // Print spaces
-        for (int j = 0; j < n - i; j++) {
-            cout << " ";
+class student{
+        int rollno;
+        string name;
+        int marks1, marks2, marks3;
+    public:
+        void get_data(){
+            cout << "Enter the name:";
+            cin >> name;
+            cout << "Enter the roll number: ";
+            cin >> rollno;
+            cout << "Enter the marks in order :";
+            cin >> marks1 >> marks2 >> marks3;
+
         }
-        // Print stars
-        for (int j = 0; j < 2 * i - 1; j++) {
-            cout << "*";
+        int total(int a,int b,int c){
+            return a+b+c;
         }
-        cout << endl;
-    }
+        void show_data(){
+            cout << "Name of the student is :" << name << endl;
+            cout << "Roll number of the student is " << rollno << endl;
+            cout << "Total marks of the 3 subjects :" << total(marks1,marks2,marks3) << endl;
+        }
+
+};
+int main(){
+    student s1;
+    s1.get_data();
+    s1.show_data();
 return 0;
 }
-
