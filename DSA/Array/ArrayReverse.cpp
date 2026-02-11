8d0eb1f26545a69c3fceaae7589a9422750ac9e1
Devanshu <devanshusingh530@gmail.com>
2026-02-11 11:57:04 +0530

    reverse of an array by two pointer approach

---
+++ DSA/Array/ArrayReverse.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void reverseArray(int arr[],int size){
+    int start=0, end =size-1;
+
+    while(start <end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+}
+int main(){
+int arr[] = {4,2,8,9,5,3,7};
+int size= 7;
+
+reverseArray(arr,size); 
+
+for(int i=0 ; i<size; i++){
+    cout << arr[i] << " ";
+}
+cout << endl;
+
+return 0;
+}
