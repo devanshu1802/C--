7044b0e6987be836b0a1e950398edad76c1cdbbc
Devanshu <devanshusingh530@gmail.com>
2026-02-12 15:24:29 +0530

    array reverse with vectors

--- DSA/Array/ArrayReverse.cpp
+++ DSA/Array/ArrayReverse.cpp
@@ -1,27 +1,42 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-void reverseArray(int arr[], int size)
-{
-    int start = 0, end = size - 1;
-    while (start < end)
-    {
-        swap(arr[start], arr[end]);
+// void reverseArray(int arr[], int size){
+//     int start = 0, end = size - 1;
+
+//     while (start < end){
+//         swap(arr[start], arr[end]);
+//         start++;
+//         end--;
+//     }
+// }
+// int main(){
+//     int arr[] = {4, 2, 8, 9, 5, 3, 7};
+//     int size = 7;
+
+//     reverseArray(arr, size);
+//     for (int i = 0; i < size; i++){
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+//     return 0;
+// }
+
+// Using vector
+void reverse(vector<int> &vec){
+    int start=0, end = (vec.size())-1;
+    while(start < end){
+        swap(vec[start], vec[end]);
         start++;
         end--;
     }
 }
-int main()
-{
-    int arr[] = {4, 2, 8, 9, 5, 3, 7};
-    int size = 7;
-
-    reverseArray(arr, size);
-    for (int i = 0; i < size; i++)
-    {
-        cout << arr[i] << " ";
+int main(){
+    vector<int> vec ={1,2,3,4,5};
+    reverse(vec);
+    for(int val: vec){
+        cout << val << " ";
     }
     cout << endl;
-
-    return 0;
 }
