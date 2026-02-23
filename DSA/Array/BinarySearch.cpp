e2770d4b4980626141d19b4a81774e17ffaae953
Devanshu <devanshusingh530@gmail.com>
2026-02-23 21:43:21 +0530

    Refactor binary search implementation for clarity and remove unnecessary whitespace

--- DSA/Array/BinarySearch.cpp
+++ DSA/Array/BinarySearch.cpp
@@ -5,7 +5,7 @@ int binarySearch(vector<int>vec , int tar){
     int st=0 , end = vec.size()-1;
 
     while(st <=end){
-        int mid = (st + (end-st))/2;
+        int mid = st + (end-st)/2;
 
         if(tar >vec[mid]){
             st = mid+1;
@@ -23,7 +23,7 @@ int main(){
     vector<int> vec = {1,2,3,4,5,7,9};
     int target = 7;
     int result = binarySearch(vec, target);
-    
+
     cout << result << endl;
 
 return 0;
