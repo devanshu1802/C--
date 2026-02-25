f44fef7b2bc68e21a808c5f143c64d0599c49377
Devanshu <devanshusingh530@gmail.com>
2026-02-25 13:14:12 +0530

    Add implementation for book allocation problem

---
+++ DSA/Array/BookAllocation.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+// Question - Book allocation problem.    
+// N - No. of books,   M - No. of students
+int allocateBooks(vector<int> &arr, int n, int m){
+    if(m>n){
+        return -1;
+    }
+
+    bool isvalid(vector<int> &arr, int n, int m,int maxAllowedPages){
+        int stu=1 , pages =0;
+        for(int i=0; i<n; i++){
+            
+        }
+    }
+    int sum = 0;
+    for(int i=0; i<n; i++){
+        sum += arr[i];
+    }
+
+    int ans = -1;
+    int st=0 , end = sum;    // Range of possible Answers
+    while(st <= end){
+        int mid = st + (end-st)/2;
+
+        if(isValid(arr,n,m,mid)){   // Left
+            ans = mid;
+            end = mid -1;
+        }
+        else{                       // Right 
+            st = mid +1;
+        }
+    }
+    return ans;
+
+}
+int main(){
+    vector<int> vec = {2,1,3,4};
+    int n =4, m=2;
+
+    cout<< allocateBooks(vec,n,m) << endl;
+
+return 0;
+}
