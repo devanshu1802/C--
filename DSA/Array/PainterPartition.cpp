d8b6b016a6d8318a05c2003aac7d320df3857878
Devanshu <devanshusingh530@gmail.com>
2026-02-25 22:12:07 +0530

    Add implementation for Painter Partition problem

---
+++ DSA/Array/PainterPartition.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+// Question - Painter Partition problem 
+bool isPossible(vector<int> &arr, int n, int m,int maxAllowedTime){
+    int painters = 1, time=0;
+
+    for(int i=0;i<n;i++){
+        if(time + arr[i] <= maxAllowedTime){
+            time += arr[i];
+        }
+        else{
+            painters++;
+            time = arr[i];
+        }
+    }
+    return painters <= m;
+}
+int minTimePaint(vector<int> &arr, int n, int m){
+    int sum=0, maxVal = INT_MIN;
+    for(int i=0; i<n; i++){
+        sum += arr[i];
+        maxVal = max(maxVal, arr[i]);
+    }
+    int st= maxVal, end = sum ,ans=-1;
+
+    while(st<= end){
+        int mid = st + (end-st)/2;
+         if(isPossible(arr,n,m,mid)){        // Left
+            ans = mid;
+            end = mid-1;
+        }
+        else{                           // Right 
+            st = mid+1;
+        }
+
+    }
+    return ans;
+}
+int main(){
+    vector<int> vec = {40,30,10,20};
+    int n =4, m=2;
+
+    cout << minTimePaint(vec,n,m) << endl;
+
+return 0;
+}
