8680bab4b191577bdecaf43bdd53c871dd082d74
Devanshu <devanshusingh530@gmail.com>
2026-03-01 20:08:49 +0530

    sorting techniques completed

--- DSA/Array/Sorting.cpp
+++ DSA/Array/Sorting.cpp
@@ -15,16 +15,27 @@ void bubbleSort(int arr[],int n){
 void selectSort(int arr[], int n){
     for(int i=0;i<n-1;i++){
         int smallestIndex = i;
-        
+
         for(int j=i+1; j<n; j++){
-            if(arr[j]>arr[smallestIndex]){
+            if(arr[j]<arr[smallestIndex]){
                 smallestIndex = j;
             }
         }
-        swap()
+        swap(arr[i],arr[smallestIndex]);
     }
 }
 
+// Insertion Sort 
+void insertSort(int arr[], int n ){
+    for(int i=1;i<n-1;i++){
+        int curr = i, prev = i-1;
+        while(prev >=0 && arr[prev]>curr){
+            arr[prev+1] = arr[prev];
+            prev--;
+        }
+        arr[prev+1] = curr;
+    }
+}
 void printArr(int arr[],int n){
     for(int i=0;i<n;i++){
         cout << arr[i] << " ";
@@ -38,6 +49,9 @@ int main(){
     printArr(arr, n);
 
     selectSort(arr,n);
+    printArr(arr,n);
+
+    insertSort(arr,n);
     printArr(arr,n);
 
 return 0;
